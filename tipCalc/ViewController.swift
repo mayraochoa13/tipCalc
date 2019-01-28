4cec548e03166b013c022b70097eab339f1220a9
MiguelAceves <maceves@csumb.edu>
2019-01-28 07:20:07 -0800

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Miguel Aceves on 1/28/19.
+//  Copyright © 2019 Mayra Ochoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
